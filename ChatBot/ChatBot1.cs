f9c361a525ff472a0cf0630cbe90771f959d95bb
hientran-sudo <hien.tran@student.fairfield.edu>
2022-03-27 14:48:21 -0400

    cmt

--- ChatBot/ChatBot1.cs
+++ ChatBot/ChatBot1.cs
@@ -12,7 +12,15 @@ namespace ChatBot
         Hashtable table = new Hashtable();
         
         public void InitializeKnownCarResponses()
-        {                     
+        {
+            table.Add("Car Model?", "2022 A 220 4MATIC Sedan");
+            table.Add("Price?", "It's $35,950");
+            table.Add("Engine?", "2.0L inline-4 turbo");
+            table.Add("Acceleration?", "7.1 sec 0-60 mph");
+            table.Add("City fuel economy?", "25 mpg");
+            table.Add("Highway fuel economy?", "35 mpg");
+            table.Add("Passenger capacity?", "It's 5");
+            table.Add("Cargo capacity?", "8.6 cu ft");
             table.Add("Guarantee?", "Sure - it's in the fine print of our contract");
             table.Add("Financing?", "Louie the loan shark will be happy to work with you");
 
