acef096318678d7f392b3911b18de620779f034c
hientran-sudo <hien.tran@student.fairfield.edu>
2022-03-28 14:54:10 -0400

    cmt

--- ChatBot/ConcreteMediator.cs
+++ ChatBot/ConcreteMediator.cs
@@ -29,23 +29,7 @@ namespace ChatBot
         public void Process(object sender, Hashtable table)
         {
             char answer;
-            //foreach (DictionaryEntry item in table)
-            //{
-            //    Console.WriteLine($"\nQ: {item.Key} (Enter 'Y' - to ask AI Assistant this question OR 'N' to Quit )");
-            //    Console.WriteLine("Your Input: ");
-            //    answer = Console.ReadKey().KeyChar;
-            //    Console.WriteLine($"\nA: {item.Value} \n");
-
-            //    Console.WriteLine("Does that answer your question? \nEnter \n 'Y' as Yes and to Continue \n 'N' as No and to Quit");
-            //    Console.WriteLine("Your Input: ");
-            //    answer = Console.ReadKey().KeyChar;
-            //    if (answer == 'N')
-            //    {
-            //        this._general.InitializeUnKnownResponses();
-            //        Console.WriteLine(">> Back to Main Menu\n");
-            //        break;
-            //    }               
-            //}
+            
             foreach (DictionaryEntry item in table)
             {
                 Console.WriteLine($"\nQ: {item.Key} \n(!) Enter \n 'y' - to ask AI Assistant that question OR \n 'n' - to Quit");
@@ -57,6 +41,7 @@ namespace ChatBot
                     Console.WriteLine("(?) Does that answer your question? \n(!) Enter \n 'y' as Yes and to Continue OR \n 'n' as No and to Quit");
                     Console.WriteLine("-->Your Input: ");
                     answer = Console.ReadKey().KeyChar;
+
                 }               
                 else if (answer == 'n')
                 {
@@ -67,7 +52,7 @@ namespace ChatBot
                 }
                 else
                 {
-                    Console.WriteLine("\n(!) Please enter a Correct Input\n");
+                    Console.WriteLine("\n(!) Please enter a Correct Input");
                     break;
                 }
             }
